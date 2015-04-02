5e653314815e0d80a3720ad444c460eec6ae9ca0
Florian Cargoët <florian.cargoet@gmail.com>
2015-04-02 10:43:38 +0200

    removed monkey-patch since the fix has been merged upstream

--- public/javascripts/tandoori-ext/ChatRoomView.js
+++ public/javascripts/tandoori-ext/ChatRoomView.js
@@ -35,46 +35,6 @@ define([
       // disable slash-commands
       sendChatRoomMessage: function (text) {
         this.createChatRoomMessage(text);
-      },
-
-      // fix occupants being shared between chat rooms
-      initialize: function () {
-          this.model.messages.on('add', this.onMessageAdded, this);
-          this.model.on('change:minimized', function (item) {
-              if (item.get('minimized')) {
-                  this.hide();
-              } else {
-                  this.maximize();
-              }
-          }, this);
-          this.model.on('destroy', function (model, response, options) {
-              this.hide().leave();
-          },
-          this);
-
-          this.occupantsview = new converse.ChatRoomOccupantsView({
-              model: new converse.ChatRoomOccupants({nick: this.model.get('nick')})
-          });
-
-          // fix browser storage id
-          var id =  b64_sha1('converse.occupants'+converse.bare_jid+this.model.get('id')+this.model.get('nick'));
-          this.occupantsview.model.id = id;
-          this.occupantsview.model.browserStorage = new Backbone.BrowserStorage[converse.storage](id);
-          // end of fix
-
-          this.occupantsview.chatroomview = this;
-          this.render();
-          this.occupantsview.model.fetch({add:true});
-          this.join(null);
-          converse.emit('chatRoomOpened', this);
-
-          this.$el.insertAfter(converse.chatboxviews.get("controlbox").$el);
-          this.model.messages.fetch({add: true});
-          if (this.model.get('minimized')) {
-              this.hide();
-          } else {
-              this.show();
-          }
       }
 
     };
