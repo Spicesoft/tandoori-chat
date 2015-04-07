1080939b0fec6660ef803ad98f93eb13f20544dc
Florian Cargoët <florian.cargoet@gmail.com>
2015-04-07 10:04:27 +0200

    fixed bug where occupants list was not correctly updated on presence changes

--- public/javascripts/src/converse-overrides/ChatRoomOccupantView.js
+++ public/javascripts/src/converse-overrides/ChatRoomOccupantView.js
@@ -16,14 +16,14 @@ define([
         return {
             // replace template
             render: function () {
-                var $new = tpl_occupant(
+                var $new = $(tpl_occupant(
                     _.extend(this.model.toJSON(), {
                         'desc_moderator': __('This user is a moderator'),
                         'desc_participant': __('This user can send messages in this room'),
                         'desc_visitor': __('This user can NOT send messages in this room'),
                         'desc_unavailable': __('This user is unavailable') // TODO_TANDOORI: i18n
                     })
-                );
+                ));
                 this.$el.replaceWith($new);
                 this.setElement($new, true);
                 return this;
