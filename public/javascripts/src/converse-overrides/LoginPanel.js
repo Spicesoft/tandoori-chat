f7bedf28f338849444898ef7ab91ac247670ab70
Florian Cargoët <florian.cargoet@gmail.com>
2015-04-08 15:04:53 +0200

    minimal login panel

--- public/javascripts/src/converse-overrides/LoginPanel.js
+++ public/javascripts/src/converse-overrides/LoginPanel.js
@@ -1,10 +1,37 @@
-define([], function () {
+define([
+    'jquery',
+    'utils',
+    'tpl!src/converse-overrides/templates/login_panel'
+], function (
+    $,
+    utils,
+    tpl_login_panel
+) {
     return function (plugin) {
         var converse = plugin.converse;
+        var __ = $.proxy(utils.__, converse);
 
         return {
-            initialize : function () {
-                this._super.initialize.apply(this, arguments);
+
+            initialize: function (cfg) {
+                cfg.$parent.html(this.$el.html(
+                    tpl_login_panel({})
+                ));
+                this.$tabs = cfg.$parent.parent().find('#controlbox-tabs');
+            },
+
+            render: function () {
+                this.$tabs.append(converse.templates.login_tab({label_sign_in: __('Sign in')}));
+                this.$el.find('input#jid').focus();
+                if (!this.$el.is(':visible')) {
+                    this.$el.show();
+                }
+                return this;
+            },
+
+            remove: function () {
+                this.$tabs.empty();
+                this.$el.parent().empty();
             }
         };
     };
